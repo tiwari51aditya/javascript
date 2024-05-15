a60a8330a89e85ba4a1efb0f81ba4e0922c51279
tiwari51aditya <tiwari51aditya@gmail.com>
2024-05-15 08:10:46 +0530

    Learning js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-console.log("Hello world with JS file");
-var a=7
-console.log("a")
+/*console.log("Hello world with JS file");
+let js ="amazing";
+if (js === "amazing") alert("js is fun");*/
+
+
+
+ console.log(5 + 8 + 9 - 3)
+ let firstName = "Aditya";
+let lastName = "Kumar";
+console.log(firstName + " " + lastName);
+const now = 2024;
+const dob = 2002;
+const age = now - dob;
+console.log(age)
